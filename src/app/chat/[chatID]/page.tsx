7b79fb103cae4defb8f870de33ed8f0140e3fb33
rejoanahmed <rejoanahmed8@gmail.com>
2023-09-15 11:28:12 +0800

    take user input

--- src/app/chat/[chatID]/page.tsx
+++ src/app/chat/[chatID]/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import { FormEvent, useState } from 'react'
+
 function ChatPage() {
+  const [message, setMessage] = useState('')
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('submit', message)
+    setMessage('')
+  }
   return (
     <div className='flex items-center justify-center h-screen'>
-      <div className='w-96 border-slate-700 border-2 p-4 rounded-md h-96'>
+      <div className='w-96 border-slate-700 border-2 p-4 rounded-md h-screen relative'>
         <div className='chat chat-start'>
           <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
@@ -28,11 +38,19 @@ function ChatPage() {
           <div className='chat-bubble'>I hate you!</div>
           <div className='chat-footer opacity-50'>Seen at 12:46</div>
         </div>
-        <input
-          type='text'
-          placeholder='Type here'
-          className='input input-bordered input-info w-full max-w-sm mt-auto inline-block'
-        />
+        <form
+          className='absolute bottom-0 left-0 right-0 flex items-center p-4'
+          onSubmit={handleSubmit}
+        >
+          <input
+            type='text'
+            placeholder='Type here'
+            className='input input-bordered input-info w-full max-w-sm mt-auto focus:ring-0 focus:outline-0'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button className='btn btn-primary ml-2'>Send</button>
+        </form>
       </div>
     </div>
   )
