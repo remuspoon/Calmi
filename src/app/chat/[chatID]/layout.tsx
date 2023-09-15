93fa6ccfa4c3df79ac57fb9462cf5c2bbff29057
rejoanahmed <rejoanahmed8@gmail.com>
2023-09-15 11:08:58 +0800

    anonymous chat

--- src/app/chat/[chatID]/layout.tsx
+++ src/app/chat/[chatID]/layout.tsx
@@ -1,5 +1,13 @@
-import { ReactNode } from 'react'
+'use client'
+import { ReactNode, use, useEffect } from 'react'
+import { useParams } from 'next/navigation'
 
 export default function ChatLayout({ children }: { children: ReactNode }) {
+  const chatID = useParams().chatID
+  useEffect(() => {
+    if (chatID === 'anonymous') {
+      // create anonymous chat and redirect to it
+    }
+  }, [chatID])
   return <div>{children}</div>
 }
--- src/app/chat/[chatID]/page.tsx
+++ src/app/chat/[chatID]/page.tsx
@@ -1,31 +1,38 @@
 function ChatPage() {
   return (
-    <div className='w-96 border-slate-700 border-2 p-4 rounded-md'>
-      <div className='chat chat-start'>
-        <div className='chat-image avatar'>
-          <div className='w-10 rounded-full'>
-            <img src='/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+    <div className='flex items-center justify-center h-screen'>
+      <div className='w-96 border-slate-700 border-2 p-4 rounded-md h-96'>
+        <div className='chat chat-start'>
+          <div className='chat-image avatar'>
+            <div className='w-10 rounded-full'>
+              <img src='/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+            </div>
           </div>
-        </div>
-        <div className='chat-header'>
-          Obi-Wan Kenobi
-          <time className='text-xs opacity-50'>12:45</time>
-        </div>
-        <div className='chat-bubble'>You were the Chosen One!</div>
-        <div className='chat-footer opacity-50'>Delivered</div>
-      </div>
-      <div className='chat chat-end'>
-        <div className='chat-image avatar'>
-          <div className='w-10 rounded-full'>
-            <img src='/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+          <div className='chat-header'>
+            Obi-Wan Kenobi
+            <time className='text-xs opacity-50'>12:45</time>
           </div>
+          <div className='chat-bubble'>You were the Chosen One!</div>
+          <div className='chat-footer opacity-50'>Delivered</div>
         </div>
-        <div className='chat-header'>
-          Anakin
-          <time className='text-xs opacity-50'>12:46</time>
+        <div className='chat chat-end'>
+          <div className='chat-image avatar'>
+            <div className='w-10 rounded-full'>
+              <img src='/images/stock/photo-1534528741775-53994a69daeb.jpg' />
+            </div>
+          </div>
+          <div className='chat-header'>
+            Anakin
+            <time className='text-xs opacity-50'>12:46</time>
+          </div>
+          <div className='chat-bubble'>I hate you!</div>
+          <div className='chat-footer opacity-50'>Seen at 12:46</div>
         </div>
-        <div className='chat-bubble'>I hate you!</div>
-        <div className='chat-footer opacity-50'>Seen at 12:46</div>
+        <input
+          type='text'
+          placeholder='Type here'
+          className='input input-bordered input-info w-full max-w-sm mt-auto inline-block'
+        />
       </div>
     </div>
   )
