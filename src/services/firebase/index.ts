38525cd88dc181ee06ad32f0313d39f0cd91d228
Remus <remuspoon@gmail.com>
2023-09-24 13:26:35 +0800

    API Key change

--- src/services/firebase/index.ts
+++ src/services/firebase/index.ts
@@ -5,12 +5,12 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCyi2LA1l2jnrA34er1rryTe0MBW8OUmbA',
-  authDomain: 'cbtchat-d05a3.firebaseapp.com',
-  projectId: 'cbtchat-d05a3',
-  storageBucket: 'cbtchat-d05a3.appspot.com',
-  messagingSenderId: '801361207938',
-  appId: '1:801361207938:web:5583badb6f3b5b05d24a9b'
+  apiKey: "AIzaSyAK5AiyhB1bnSHLXmI1zh-h0FiJHleAO6g",
+  authDomain: "cbtbot-e0da3.firebaseapp.com",
+  projectId: "cbtbot-e0da3",
+  storageBucket: "cbtbot-e0da3.appspot.com",
+  messagingSenderId: "812528510538",
+  appId: "1:812528510538:web:73ff2981f707c8f9fca8b3"
 }
 
 // Initialize Firebase
