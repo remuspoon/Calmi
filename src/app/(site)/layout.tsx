24fe371346718c271efc3919f0a757d5578abae0
rejoanahmed <rejoanahmed8@gmail.com>
2023-09-25 15:25:48 +0800

    routing progress

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import ChatButton from '@/components/ChatButton'
 import MyChatButton from '@/components/MyChatButton'
+import NextTopLoader from 'nextjs-toploader'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
           ' flex flex-col justify-center items-center min-h-screen'
         }
       >
+        <NextTopLoader />
         <UserProvider>
           <nav className='sticky z-10 bg-slate-900 bg-opacity-25 backdrop-blur top-0 w-screen flex items-center gap-2 px-5 py-3 border-b border-slate-600 shadow-lg shadow-slate-800'>
             <Link href={'/'} className='flex gap-2'>
--- src/components/ChatButton.tsx
+++ src/components/ChatButton.tsx
@@ -4,22 +4,28 @@ import { useRouter } from 'next/navigation'
 import { useUser } from './UserProvider'
 import { SigninWithGoogle } from '@/services/firebase/auth'
 import { createChat } from '@/services/firebase/firestore'
+import { useState } from 'react'
 
 function ChatButton({ label }: { label: string }) {
   const router = useRouter()
   let user = useUser()
+  const [disabled, setDisabled] = useState(false)
   const topNavLabel = user ? 'New Chat' : 'Sign in to chat'
   const handleClick = async () => {
+    setDisabled(true)
     if (!user || user === 'loading') {
       user = await SigninWithGoogle()
     }
     const chat = await createChat(user.uid)
     router.push(`/chat/${chat.id}`)
+
+    setDisabled(false)
   }
   return (
     <button
       className='btn btn-accent btn-outline btn-sm sm:btn-md'
       onClick={handleClick}
+      disabled={disabled}
     >
       {label === 'topNav' ? topNavLabel : label}
     </button>
