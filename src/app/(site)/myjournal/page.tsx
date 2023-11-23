3f383a446ca41e615d8360c3fc336b0b639119f9
rejoanahmed <rejoanahmed8@gmail.com>
2023-11-23 19:32:07 +0800

    journal page

--- src/app/(site)/myjournal/page.tsx
+++ src/app/(site)/myjournal/page.tsx
@@ -7,21 +7,28 @@ import { useEffect, useState } from 'react'
 function Page() {
   const user = useUser()
   const [journals, setJournals] = useState<any[]>([])
+  const [loading, setLoading] = useState<boolean>(true)
   useEffect(() => {
     if (user && user !== 'loading') {
+      setLoading(true)
       getCompletedChats(user.uid).then((chats) => {
+        console.log(chats)
         setJournals(chats)
+        setLoading(false)
       })
     }
   }, [user])
 
   if (!user || user === 'loading') return <div>Loading...</div>
-  if (journals.length === 0) return <div>No journals</div>
+  if (journals.length === 0 && !loading) return <div>No journals</div>
+
+  loading && <div>Loading...</div>
+
   return (
-    <div className='flex flex-wrap'>
-      {journals.map((journal) => {
-        return <ChatCard chat={journal} key={journal.id} />
-      })}
+    <div className='flex container mx-auto flex-wrap gap-2'>
+      {journals.map((journal) => (
+        <ChatCard chat={journal} key={journal.id} />
+      ))}
     </div>
   )
 }
