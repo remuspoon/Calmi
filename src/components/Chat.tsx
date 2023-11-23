1756f6f5364c4665521bfad01673b1b290c57ae8
rejoanahmed <rejoanahmed8@gmail.com>
2023-11-23 18:51:28 +0800

    icon

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -239,25 +239,13 @@ function Chat() {
               <div key={index} className='chat chat-start break-inside-avoid'>
                 <div className='chat-image avatar'>
                   <div className='w-10 rounded-full bg-slate-700 flex'>
-                    <svg
-                      xmlns='http://www.w3.org/2000/svg'
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      stroke='currentColor'
-                      strokeWidth='2'
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      className='mt-2 ml-2 text-green-500'
-                    >
-                      <path d='M12 8V4H8' />
-                      <rect width='16' height='12' x='4' y='8' rx='2' />
-                      <path d='M2 14h2' />
-                      <path d='M20 14h2' />
-                      <path d='M15 13v2' />
-                      <path d='M9 13v2' />
-                    </svg>
+                    <Image
+                      src={'/icon.png'}
+                      width={20}
+                      height={20}
+                      className='bg-primary'
+                      alt='bot icon'
+                    />
                   </div>
                 </div>
                 {/* <div className='chat-header'>
